15d030b1aa7ccfc2148f676f8fad616815ee84b3
ismum <ichowdhury4@fordham.edu>
2021-04-26 13:35:56 -0400

    pw hashing added

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { userInputError, UserInputError } = require('apollo-server');
+
+const { SECRET_KEY } = require('../../config')
+const User = require('../../models/User');
+
+module.exports = {
+    Mutation: {
+        async register(
+        _, 
+        { 
+         registerInput: { username, email, password, confirmPassword}
+        }, 
+        context,
+        info
+         ){
+            // todo validate user data
+            // make sure user doesnt already exist
+
+            const user= User.findOne({ username });
+            if (user){
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'this username is taken'
+                    }
+                })
+            }
+            password = await bcrypt.hash(password,12);
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            });
+
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+    }
+
+}
